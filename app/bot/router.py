900def8225c9ffcf3c1001aba51ceaca7beed1f1
ofry <speitshot@gmail.com>
2022-04-16 21:48:30 +0300

    Add handler for delete record

--- app/bot/router.py
+++ app/bot/router.py
@@ -1,9 +1,11 @@
 from aiogram import Dispatcher
 
+from bot.main_menu import add_handlers as main_handlers
 from bot.record.handlers import add_handlers as record_handlers
 from bot.records.handlers import add_handlers as records_handlers
 
 
 def add_handlers(dp: Dispatcher):
+    main_handlers(dp)
     record_handlers(dp)
     records_handlers(dp)
