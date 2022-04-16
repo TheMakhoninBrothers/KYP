180f534e0461e9258883b35ddf477d240b083330
ofry <speitshot@gmail.com>
2022-04-16 13:18:02 +0300

    Add func startup with init tables in db

--- app/__main__.py
+++ app/__main__.py
@@ -1,6 +1,13 @@
 from aiogram import executor
 
+import db.postgres as db
 from bot import dp
 
+
+async def on_startup(_):
+    async with db.async_engine.begin() as conn:
+        await conn.run_sync(db.Base.metadata.create_all)
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
