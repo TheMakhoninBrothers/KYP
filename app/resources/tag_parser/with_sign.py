37c9c759f1255a5d12057b70c030feff0c868717
ofry <speitshot@gmail.com>
2022-04-16 19:31:02 +0300

    Rename parser for add record

--- app/resources/tag_parser/with_sign.py
+++ app/resources/tag_parser/with_sign.py
@@ -3,7 +3,7 @@ import typing
 from interfaces.resources.tag_parser import TagParser
 
 
-class TagParserFromTelegram(TagParser):
+class TagParserWithSign(TagParser):
 
     def parse(self, message_text: str) -> typing.List[str]:
         return [word.replace('#', '').lower() for word in message_text.split(' ') if word.startswith('#')]
