3ca89886a7c043e14b0c008bdf647b572bbdf466
ofry <speitshot@gmail.com>
2022-04-16 13:14:49 +0300

    Add configs and new env variables

--- app/configs.py
+++ app/configs.py
@@ -5,3 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.environ['BOT_TOKEN']
+
+POSTGRES_USER = os.environ['POSTGRES_USER']
+POSTGRES_PASS = os.environ['POSTGRES_PASS']
+POSTGRES_HOST = os.environ['POSTGRES_HOST']
+POSTGRES_PORT = os.environ['POSTGRES_PORT']
+POSTGRES_DBNAME = os.environ['POSTGRES_DBNAME']
+
+POSTGRES_URI = f'postgresql+asyncpg://{POSTGRES_PASS}:{POSTGRES_PASS}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DBNAME}'
